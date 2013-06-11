fe415c5e6669c8a5970b4a0ea925ff5f6027114d
Martin Zottmann <github@eiscp.de>
2013-06-11 10:59:15 +0200

    Automatic vertex attribute pointers

--- Window and Graphics/Graphics/OpenGL/VertexArrayObject.cs
+++ Window and Graphics/Graphics/OpenGL/VertexArrayObject.cs
@@ -16,63 +16,38 @@ namespace MartinZottmann.Graphics.OpenGL
 
         public void Add<U>(BufferObject<U> bo) where U : struct
         {
-            //FieldInfo[] fi = bo.data[0].GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
-            //foreach (FieldInfo info in fi)
-            //{
-            //    Console.WriteLine(info);
-            //}
             using (new Bind(this))
             {
                 switch (bo.target)
                 {
                     case BufferTarget.ArrayBuffer:
-                        if (bo.data is VertexP3C4[])
+                        using (new Bind(bo)) // to VertexAttribPointer
                         {
                             var vertex_attribute = 0;
                             var offset = 0;
                             var stride = BlittableValueType.StrideOf(bo.data);
 
-                            using (new Bind(bo)) // to VertexAttribPointer
+                            FieldInfo[] fi = bo.data[0].GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+                            foreach (FieldInfo info in fi)
                             {
-                                // @todo get vertex_attibute from program/shader via GL.GetAttribLocation(program.id, ...)
-                                GL.VertexAttribPointer(vertex_attribute, 3, VertexAttribPointerType.Float, false, stride, offset);
-                                GL.EnableVertexAttribArray(vertex_attribute);
-                                vertex_attribute++;
-                                offset += sizeof(float) * 3;
+                                int size = 0;
+                                VertexAttribPointerType type = VertexAttribPointerType.Byte;
+                                foreach (FieldInfo info2 in info.FieldType.GetFields(BindingFlags.Public | BindingFlags.Instance))
+                                {
+                                    size++;
+                                    switch (Type.GetTypeCode(info2.FieldType))
+                                    {
+                                        case TypeCode.Single: type = VertexAttribPointerType.Float; break;
+                                        default: throw new NotImplementedException();
+                                    }
+                                }
 
-                                GL.VertexAttribPointer(vertex_attribute, 4, VertexAttribPointerType.Float, false, stride, offset);
+                                GL.VertexAttribPointer(vertex_attribute, size, type, false, stride, offset);
                                 GL.EnableVertexAttribArray(vertex_attribute);
                                 vertex_attribute++;
-                                offset += sizeof(float) * 4;
+                                offset += System.Runtime.InteropServices.Marshal.SizeOf(info.FieldType);
                             }
                         }
-                        else if (bo.data is VertexP3N3T2[])
-                        {
-                            var vertex_attribute = 0;
-                            var offset = 0;
-                            var stride = BlittableValueType.StrideOf(bo.data);
-
-                            using (new Bind(bo)) // to VertexAttribPointer
-                            {
-                                // @todo get vertex_attibute from program/shader via GL.GetAttribLocation(program.id, ...)
-                                GL.VertexAttribPointer(vertex_attribute, 3, VertexAttribPointerType.Float, false, stride, offset);
-                                GL.EnableVertexAttribArray(vertex_attribute);
-                                vertex_attribute++;
-                                offset += sizeof(float) * 3;
-
-                                GL.VertexAttribPointer(vertex_attribute, 3, VertexAttribPointerType.Float, false, stride, offset);
-                                GL.EnableVertexAttribArray(vertex_attribute);
-                                vertex_attribute++;
-                                offset += sizeof(float) * 3;
-
-                                GL.VertexAttribPointer(vertex_attribute, 2, VertexAttribPointerType.Float, false, stride, offset);
-                                GL.EnableVertexAttribArray(vertex_attribute);
-                                vertex_attribute++;
-                                offset += sizeof(float) * 2;
-                            }
-                        }
-                        else
-                            throw new NotImplementedException();
                         break;
                     case BufferTarget.ElementArrayBuffer:
                         bo.Bind(); // Unbinding will result in an address violation
