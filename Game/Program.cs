8efa2992c9b7581e9d00831fdfe8af39b5dbfa0f
Martin Zottmann <github@eiscp.de>
2013-07-02 09:11:31 +0200

    Fix project layout

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,19 +1,15 @@
-﻿using MartinZottmann.Game;
-using OpenTK.Graphics;
+﻿using OpenTK.Graphics;
 using System;
 
-namespace MartinZottmann
+namespace MartinZottmann.Game
 {
     public class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-            GraphicsMode mode = new GraphicsMode(new OpenTK.Graphics.ColorFormat(32), 24, 0, 2, new OpenTK.Graphics.ColorFormat(32));
-            using (Window game = new Window(mode))
-            {
+            using (Window game = new Window(new GraphicsMode(new ColorFormat(32), 24, 0, 2, new ColorFormat(32))))
                 game.Run();
-            }
 #if DEBUG
             Console.ReadKey();
 #endif
