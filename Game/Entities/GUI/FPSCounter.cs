9838b3473b767d2497026cf6f5ea31ccaa7468ff
Martin Zottmann <github@eiscp.de>
2013-09-25 19:39:35 +0200

    Add distinct update and render FPS counters to GUI

--- Game/Entities/GUI/FPSCounter.cs
+++ Game/Entities/GUI/FPSCounter.cs
@@ -8,11 +8,32 @@ namespace MartinZottmann.Game.Entities.GUI
 {
     public class FPSCounter : Abstract
     {
-        protected double accumulator = 0;
+        public class Counter
+        {
+            protected double accumulator = 0;
 
-        protected int counter = 0;
+            protected int counter = 0;
 
-        protected int fps = 0;
+            protected int fps = 0;
+
+            public int FPS { get { return fps; } }
+
+            public void Tick(double delta_time)
+            {
+                counter++;
+                accumulator += delta_time;
+                if (accumulator > 1)
+                {
+                    fps = counter;
+                    counter = 0;
+                    accumulator -= 1;
+                }
+            }
+        }
+
+        protected Counter update = new Counter();
+
+        protected Counter render = new Counter();
 
         public override void Bind(ResourceManager resource_manager, FontMeshBuilder font_mesh_builder)
         {
@@ -25,21 +46,20 @@ namespace MartinZottmann.Game.Entities.GUI
 
         public override void Update(double delta_time)
         {
-            counter++;
-            accumulator += delta_time;
-            if (accumulator > 1)
-            {
-                if (fps != counter)
-                    RebuildModel();
-                fps = counter;
-                counter = 0;
-                accumulator -= 1;
-            }
+            update.Tick(delta_time);
+            base.Update(delta_time);
+        }
+
+        public override void Render(double delta_time)
+        {
+            render.Tick(delta_time);
+            RebuildModel();
+            base.Render(delta_time);
         }
 
         protected void RebuildModel()
         {
-            Model.Mesh(font_mesh_builder.FromString(String.Format("FPS: {0:F}", counter)));
+            Model.Mesh(font_mesh_builder.FromString(String.Format("FPS: {0:F}/{1:F}", update.FPS, render.FPS)));
         }
     }
 }
